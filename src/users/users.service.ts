6d82cb30eccc49343c608ce5c36b9e15f4cddd6d
LonNV <nguyenvanlon444@gmail.com>
2024-05-27 13:33:59 +0900

    Add create function in user service and inject into controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository} from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    // @InjectRepository(User) dùng để inject đối tượng repo có kiểu Repository<User> vào Repository để sử dụng các mothed trong repository mà TypeORM cung cấp
+    constructor(@InjectRepository(User) private repo: Repository<User>){}
+
+    create(email: string, password: string){
+        const user = this.repo.create({email, password});
+
+        return this.repo.save(user);
+    }
+}
