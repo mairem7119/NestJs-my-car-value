085b426ff20d03d509d0e8d41fff0971983b9b10
LonNV <nguyenvanlon444@gmail.com>
2024-05-27 15:52:35 +0900

    Complete service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import { Repository} from 'typeorm';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-    // @InjectRepository(User) dùng để inject đối tượng repo có kiểu Repository<User> vào Repository để sử dụng các mothed trong repository mà TypeORM cung cấp
-    constructor(@InjectRepository(User) private repo: Repository<User>){}
+  // @InjectRepository(User) dùng để inject đối tượng repo có kiểu Repository<User> vào Repository để sử dụng các mothed trong repository mà TypeORM cung cấp
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-    create(email: string, password: string){
-        const user = this.repo.create({email, password});
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
 
-        return this.repo.save(user);
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id: id } });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email: email } });
+  }
+
+  async update(id: number,  attrs: Partial<User>) {
+    const user = await this.repo.findOne({where: {id: id}});
+    if(!user){
+        throw new Error('user not found');
     }
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne({where: {id: id}});
+    if(!user){
+        throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
