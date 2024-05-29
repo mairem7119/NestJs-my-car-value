92a210cdcfe39f052bd042bbee850d54442b960b
LonNV <nguyenvanlon444@gmail.com>
2024-05-29 11:42:11 +0900

    Add Interceptors to not send password when query DB 1.0

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import { Entity, Column, AfterUpdate, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
-export class User{
-    @PrimaryGeneratedColumn()
-    id: number;
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  @Exclude() //Ngăn việc tải password về khi thực hiện truy vấn dữ liệu khi dùng TypeORM
+  password: string;
+
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,9 @@ import {
   Query,
   Delete,
   Put,
-  NotFoundException
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -23,6 +25,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
